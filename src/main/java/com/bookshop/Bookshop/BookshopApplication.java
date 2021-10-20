cbcf6e6883a4ed4cc5f93d472756d272c4b6ec34
Сергей Луговых <lugowyh.serezha@yandex.ru>
2021-10-20 16:50:33 +0500

    added proj that does not work

--- src/main/java/com/bookshop/Bookshop/BookshopApplication.java
+++ src/main/java/com/bookshop/Bookshop/BookshopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookshopApplication {
 
 	public static void main(String[] args) {
+		System.out.println("HelloWOrld!");
 		SpringApplication.run(BookshopApplication.class, args);
 	}
-
 }
